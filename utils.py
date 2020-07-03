14c278ed277df4fda5833543fa052e7c3dcd8d40
Gaozhongpai <gaozhongpai@gmail.com>
2020-07-03 19:09:25 +0800

    clear

--- utils.py
+++ utils.py
@@ -7,10 +7,7 @@ import torch
 import math
 from collections import deque
 import scipy.sparse as sp
-## Note: Non-smooth surfaces or bad triangulations may lead to non-spiral orderings of the vertices.
-## Common issue in badly triangulated surfaces is that there exist some edges that belong to more than two triangles. In this
-## case the mathematical definition of the spiral is insufficient. In this case, in this version of the code, we randomly
-## choose two triangles in order to continue the inductive assignment of the order to the rest of the vertices.
+
 
 def normalize(mx):
     """Row-normalize sparse matrix"""
